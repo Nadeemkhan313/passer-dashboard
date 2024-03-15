cdc2fcfc7dfe4e0681251dac300614c961a29aa1
Nadeem khan <Nadeemkhanpathan80@gmail.com>
2024-03-15 15:12:31 +0530

    changed

---
+++ src/data/index.js
@@ -0,0 +1,84 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Rohit',
+    lastName: 'shinde',
+    email: 'RS@example.com',
+    salary: '95000',
+    date: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'Arjun',
+    lastName: 'Patil',
+    email: 'AP@example.com',
+    salary: '80000',
+    date: '2019-04-17'
+  },
+  {
+    id: 3,
+    firstName: 'Harsh',
+    lastName: 'Rane',
+    email: 'HR@example.com',
+    salary: '79000',
+    date: '2019-05-01'
+  },
+  {
+    id: 4,
+    firstName: 'sudheer',
+    lastName: 'Patil',
+    email: 'SP@example.com',
+    salary: '56000',
+    date: '2019-05-03'
+  },
+  {
+    id: 5,
+    firstName: 'Mohit',
+    lastName: 'rao',
+    email: 'MR@example.com',
+    salary: '65000',
+    date: '2019-06-13'
+  },
+  {
+    id: 6,
+    firstName: 'sumeet',
+    lastName: 'jadhav',
+    email: 'sj@example.com',
+    salary: '120000',
+    date: '2019-07-30'
+  },
+  {
+    id: 7,
+    firstName: 'Pradip',
+    lastName: 'mane',
+    email: 'pm@example.com',
+    salary: '90000',
+    date: '2019-08-15'
+  },
+  {
+    id: 8,
+    firstName: 'dinesh',
+    lastName: 'patil',
+    email: 'dp@example.com',
+    salary: '60000',
+    date: '2019-10-10'
+  },
+  {
+    id: 9,
+    firstName: 'arjun',
+    lastName: 'shinde',
+    email: 'as@example.com',
+    salary: '71000',
+    date: '2019-10-15'
+  },
+  {
+    id: 10,
+    firstName: 'Anirudh',
+    lastName: 'maske',
+    email: 'Am@example.com',
+    salary: '110000',
+    date: '2020-01-15'
+  }
+];
+
+export { employeesData };
